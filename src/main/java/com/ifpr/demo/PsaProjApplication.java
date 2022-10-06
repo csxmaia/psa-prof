9aa7688b84ba5a18cf6b2794ce47d171e7374f4a
Cristhian <cristhian.smaia@gmail.com>
2022-10-06 20:50:57 -0300

    first

---
+++ src/main/java/com/ifpr/demo/PsaProjApplication.java
@@ -0,0 +1,13 @@
+package com.ifpr.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsaProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsaProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ifpr/demo/PsaProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifpr.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsaProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
