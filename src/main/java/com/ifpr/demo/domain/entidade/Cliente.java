12ebe7672a2ddd1b656dfc4b4cb31dda5ebf8edb
IgorLuigi <igor.luigi@hotmail.com>
2022-10-06 22:19:15 -0300

    Cliente

--- src/main/java/com/ifpr/demo/domain/entidade/Cliente.java
+++ src/main/java/com/ifpr/demo/domain/entidade/Cliente.java
@@ -1,10 +1,40 @@
 public class Cliente {
-	private long id;
+	private String nome;
 	private String email;
 	private String telefone;
 	private String cpf;
 	private CNH cnh;
 	private Date dataNascimento;
+	
+	public Cliente(String nome, String email, String telefone, String cpf, CNH cnh, Date dataNascimento ) {
+		this.nome = nome;
+		this.email = email;
+		this.telefone = telefone;
+		this.cpf = cpf;
+		this.cnh = cnh;
+		this.dataNascimento = dataNascimento;
+	}
+	
+	public void cadastrar(DadosCliente dadosCliente) throws validarIdade {
+		String nome = dadosCliente.getNome();
+		String email = dadosCliente.getEmail();
+		String telefone = dadosCliente.getTelefone();
+		String cpf = dadosCliente.getCpf();
+		
+		if(validaridade()) {
+			throw new validarIdade();
+		}
+		if(validarCNH()) {
+			throw new validarCNH();
+		}
+		
+		this.nome = nome;
+		this.email = email;
+		this.telefone = telefone;
+		this.cpf = cpf;
+		this.cnh = cnh;
+		this.dataNascimento = dataNascimento;
+	}
 	
 	private boolean validarIdade() {
 		int anoAtual = DateTime.now().year;
