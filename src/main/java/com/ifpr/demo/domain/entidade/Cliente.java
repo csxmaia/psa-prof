75f2ab935fcd8a2f13f8f54b9831eedd90d24d04
IgorLuigi <igor.luigi@hotmail.com>
2022-10-06 22:06:07 -0300

    Mirei no cardoso e acertei o ** dele

--- src/main/java/com/ifpr/demo/domain/entidade/Cliente.java
+++ src/main/java/com/ifpr/demo/domain/entidade/Cliente.java
@@ -1,4 +1,21 @@
 public class Cliente {
-	private long id
-	private 
+	private long id;
+	private String email;
+	private String telefone;
+	private String cpf;
+	private CNH cnh;
+	private Date dataNascimento;
+	
+	private boolean validarIdade() {
+		int anoAtual = DateTime.now().year;
+
+	    int anoNascimento = dataNascimento.year;
+
+	    int idade = anoAtual - anoNascimento;
+	    if (idade >= 21) {
+	      return true;
+	    } else {
+	      return false;
+	    }
+	}
 }
